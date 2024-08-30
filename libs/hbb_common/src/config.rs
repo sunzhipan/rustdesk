ad508f8310389f884407f5ff3603eae2c82f4259
sunzhipan <75620059+sunzhipan@users.noreply.github.com>
2024-08-30 14:57:01 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -102,7 +102,7 @@ const CHARS: &[char] = &[
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["szppp.iask.in"];
 pub const PUBLIC_RS_PUB_KEY: &str = "KJxQTiAmAqnAEK42isjP7UI1eRQTjoOZeiLpBFPQETc=";
-
+pub const API_SERVER: &[&str] = &["http://szppp.iask.in:21114"];
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
